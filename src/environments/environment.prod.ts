c6eff8b794ef375601ce96339975b18e95599249
SaiPrahlad6 <krishnaprahladm@gmail.com>
2021-06-06 13:55:37 +0530

    Changed schema

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyCTLBw6MEJeFx7NQ8naKlLbfqan05bw2I8",
+    authDomain: "benchmate-4917b.firebaseapp.com",
+    databaseURL:"https://benchmate-4917b-default-rtdb.firebaseio.com/",
+    projectId: "benchmate-4917b",
+    storageBucket: "benchmate-4917b.appspot.com",
+    messagingSenderId: "595432038188",
+    appId: "1:595432038188:web:b4bfd0367043fa08719689",
+    measurementId: "G-ZZ9LBLSM6D"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,25 @@
 
 export const environment = {
   production: false,
+  // firebase : {
+  //   apiKey: "AIzaSyAoZ7wEaCH4zcF_ShHf6Y_DSfbyQN_c5ok",
+  //   authDomain: "noted-c0aea.firebaseapp.com",
+  //   databaseURL: "https://noted-c0aea-default-rtdb.firebaseio.com",
+  //   projectId: "noted-c0aea",
+  //   storageBucket: "noted-c0aea.appspot.com",
+  //   messagingSenderId: "753679379335",
+  //   appId: "1:753679379335:web:713e17c62f3cee22ec8122",
+  //   measurementId: "G-N19XFEVY6R"
+  // }
   firebase : {
-    apiKey: "AIzaSyAoZ7wEaCH4zcF_ShHf6Y_DSfbyQN_c5ok",
-    authDomain: "noted-c0aea.firebaseapp.com",
-    databaseURL: "https://noted-c0aea-default-rtdb.firebaseio.com",
-    projectId: "noted-c0aea",
-    storageBucket: "noted-c0aea.appspot.com",
-    messagingSenderId: "753679379335",
-    appId: "1:753679379335:web:713e17c62f3cee22ec8122",
-    measurementId: "G-N19XFEVY6R"
+    apiKey: "AIzaSyCTLBw6MEJeFx7NQ8naKlLbfqan05bw2I8",
+    authDomain: "benchmate-4917b.firebaseapp.com",
+    databaseURL:"https://benchmate-4917b-default-rtdb.firebaseio.com/",
+    projectId: "benchmate-4917b",
+    storageBucket: "benchmate-4917b.appspot.com",
+    messagingSenderId: "595432038188",
+    appId: "1:595432038188:web:b4bfd0367043fa08719689",
+    measurementId: "G-ZZ9LBLSM6D"
   }
 };
 
